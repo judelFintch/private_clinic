eb081981184f0d9553de5d07602a54531d30f7dd
chriskk <christiankiyumbik@gmail.com>
2021-11-23 09:31:21 +0100

    modelisation modified

---
+++ confg/config.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+$dataConfig = file_get_contents("config/config.txt");
+if(empty($dataConfig)){
+    ?>
+        <script>window.location.replace("install.php")</script>
+    <?php
+}
+$dataConfig = explode(';', $dataConfig);
+
+if (!defined('DB_SERVER')) define('DB_SERVER', $dataConfig[0]);
+if (!defined('DB_USER')) define('DB_USER', $dataConfig[1]);
+if (!defined('DB_PWD')) define('DB_PWD', $dataConfig[2]);
+if (!defined('DB_NAME')) define('DB_NAME', $dataConfig[3]);
+if (!defined('DSN')) define('DSN', 'mysql:host='.DB_SERVER.'; dbname='.DB_NAME);
