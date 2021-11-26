16ac14a1c169c18c4ea9e555a72d2f175e814f37
judel fintch <judfintch@gmail.com>
2021-11-26 16:53:09 +0100

    remove text default page

--- pages/default.php
+++ pages/default.php
@@ -8,8 +8,8 @@
             <div class="col-md-12 grid-margin">
               <div class="row">
                 <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                  <h3 class="font-weight-bold">Welcome Aamir</h3>
-                  <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span class="text-primary">3 unread alerts!</span></h6>
+                  <h3 class="font-weight-bold"> Bashboard <?=$logiciel_client?> / <?=$logiciel_name?></h3>
+                  <h6 class="font-weight-normal mb-0"></span></h6>
                 </div>
                 <div class="col-12 col-xl-4">
                  <div class="justify-content-end d-flex">
@@ -33,16 +33,10 @@
             <div class="col-md-6 grid-margin stretch-card">
               <div class="card tale-bg">
                 <div class="card-people mt-auto">
-                  <img src="images/dashboard/people.svg" alt="people">
+                  
                   <div class="weather-info">
                     <div class="d-flex">
-                      <div>
-                        <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2>
-                      </div>
-                      <div class="ml-2">
-                        <h4 class="location font-weight-normal">Bangalore</h4>
-                        <h6 class="font-weight-normal">India</h6>
-                      </div>
+                    
                     </div>
                   </div>
                 </div>
@@ -53,7 +47,7 @@
                 <div class="col-md-6 mb-4 stretch-card transparent">
                   <div class="card card-tale">
                     <div class="card-body">
-                      <p class="mb-4">Today’s Bookings</p>
+                      <p class="mb-4">Hopistaliser</p>
                       <p class="fs-30 mb-2">4006</p>
                       <p>10.00% (30 days)</p>
                     </div>
@@ -62,8 +56,8 @@
                 <div class="col-md-6 mb-4 stretch-card transparent">
                   <div class="card card-dark-blue">
                     <div class="card-body">
-                      <p class="mb-4">Total Bookings</p>
-                      <p class="fs-30 mb-2">61344</p>
+                      <p class="mb-4">Consulter</p>
+                      <p class="fs-30 mb-2">61</p>
                       <p>22.00% (30 days)</p>
                     </div>
                   </div>
@@ -73,8 +67,8 @@
                 <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
                   <div class="card card-light-blue">
                     <div class="card-body">
-                      <p class="mb-4">Number of Meetings</p>
-                      <p class="fs-30 mb-2">34040</p>
+                      <p class="mb-4">Maternite</p>
+                      <p class="fs-30 mb-2">34</p>
                       <p>2.00% (30 days)</p>
                     </div>
                   </div>
@@ -82,8 +76,8 @@
                 <div class="col-md-6 stretch-card transparent">
                   <div class="card card-light-danger">
                     <div class="card-body">
-                      <p class="mb-4">Number of Clients</p>
-                      <p class="fs-30 mb-2">47033</p>
+                      <p class="mb-4">Patients</p>
+                      <p class="fs-30 mb-2">33</p>
                       <p>0.22% (30 days)</p>
                     </div>
                   </div>
